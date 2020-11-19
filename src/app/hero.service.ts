6773d4635d62c60b0b70a70ae117d55ae52ec889
hellonathanchung <chung.nathanh@gmail.com>
2020-11-18 18:45:46 -0800

    updates heroes component

---
+++ src/app/hero.service.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+
+  constructor() { }
+
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
+
+}
