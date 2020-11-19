1c03464b0417a8226eff40af60775434f4bc3a78
hellonathanchung <chung.nathanh@gmail.com>
2020-11-19 07:17:12 -0800

    adds functionality for getting data from a server

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,32 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
-import { HeroService } from '../hero.service';
 
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[];
-    selectedHero: Hero;
 
-    constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService) { }
 
-    ngOnInit() {
-      this.getHeroes();
-    }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  ngOnInit() {
+    this.getHeroes();
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 }
